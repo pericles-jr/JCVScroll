69b5cd350e5ab7f89ad7d5971785394f9d2130f6
Pericles Jr <periclesjr@Mac-mini-Pericles.local>
2018-03-01 09:28:14 -0300

    first commit

---
+++ Example/JCVScroll/SimpleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SimpleTableViewCell.swift
+//  JCVScroll_Example
+//
+//  Created by Pericles Jr on 28/02/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class SimpleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
